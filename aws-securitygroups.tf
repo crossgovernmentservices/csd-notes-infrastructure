732e8535499e9aded8b2126c02239cf4ca8ed5fc
Kerin Cosford <cosford@gmail.com>
2016-02-11 22:29:04 +0000

    web instances only accept HTTP from ELB

--- aws-securitygroups.tf
+++ aws-securitygroups.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    security_groups = ["${aws_security_group.elb.id}"]
   }
 
   # outbound internet access
