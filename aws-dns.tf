3978799fe7d1b1c3409101b661f857ece037d02c
Kerin Cosford <cosford@gmail.com>
2016-04-05 09:55:28 +0100

    Added DNS for web ELB

--- aws-dns.tf
+++ aws-dns.tf
@@ -1,16 +1,12 @@
-resource "aws_route53_zone" "dev" {
-  name = "dev.civilservice.digital"
-  tags {
-    Environment = "dev"
-  }
-}
+# Public A record
+resource "aws_route53_record" "public-a" {
+  zone_id = "${var.r53_notes_zone_id}"
+  name = "${var.domain_prefix}"
+  type = "A"
 
-resource "aws_route53_record" "ssh" {
-    zone_id = "${aws_route53_zone.dev.zone_id}"
-    name = "ssh.dev.civilservice.digital"
-    type = "A"
-    ttl = "300"
-    records = [
-       "${aws_instance.ssh-bastion.public_ip}"
-    ]
+  alias {
+    name = "${aws_elb.web.dns_name}"
+    zone_id = "${aws_elb.web.zone_id}"
+    evaluate_target_health = true
+  }
 }
