973e4f3968d0479b16bbe60753180ec991d234d1
Kerin Cosford <cosford@gmail.com>
2016-04-05 10:00:38 +0100

    Added ELB name, web SG, subnets and env name to tf outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,32 @@
-output "address" {
+output "elb_address" {
   value = "${aws_elb.web.dns_name}"
+}
+
+output "elb_name" {
+  value = "${aws_elb.web.name}"
+}
+
+
+output "web_sg_id" {
+    value = "${aws_security_group.web.id}"
+}
+
+output "availability_zones" {
+    value = "${join(",", aws_elb.web.availability_zones)}"
+}
+
+output "public_subnets" {
+    value = "${join(",", aws_elb.web.subnets)}"
+}
+
+output "private_subnets" {
+    value = "${aws_subnet.private-a.id},${aws_subnet.private-b.id},${aws_subnet.private-c.id}"
+}
+
+output "environment" {
+    value = "${var.environment}"
+}
+
+output "rds_main_address" {
+    value = "${aws_db_instance.main.address}"
 }
