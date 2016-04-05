50334c1f9b104f7e4949d2a348ca874b7c79b4a3
Kerin Cosford <cosford@gmail.com>
2016-04-05 10:02:45 +0100

    Update variables with new AMI, env name, domain_prefix and main DNS zone ID

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
-variable "public_key_path" {
-  description = <<DESCRIPTION
-Path to the SSH public key to be used for authentication.
-Ensure this keypair is added to your local SSH agent so provisioners can
-connect.
+# variable "public_key_path" {
+#   description = <<DESCRIPTION
+# Path to the SSH public key to be used for authentication.
+# Ensure this keypair is added to your local SSH agent so provisioners can
+# connect.
 
-Example: ~/.ssh/terraform.pub
-DESCRIPTION
-}
+# Example: ~/.ssh/terraform.pub
+# DESCRIPTION
+# }
 
 variable "key_name" {
   description = "Desired name of AWS key pair"
@@ -18,9 +18,31 @@ variable "aws_region" {
 }
 
 # Ubuntu Precise 14.04 LTS (x64)
-variable "aws_amis" {
+variable "aws_web_amis" {
   default = {
-    eu-west-1 = "ami-f95ef58a"
+    eu-west-1 = "ami-dfbc00ac"
     eu-central-1 = "ami-87564feb"
   }
 }
+
+variable "aws_mongo_amis" {
+  default = {
+    eu-west-1 = "ami-e3cf7390"
+    eu-central-1 = "ami-87564feb"
+  }
+}
+
+variable "environment" {
+  description = "Deployment environment, e.g. staging, production etc."
+  default = "dev"
+}
+
+variable "domain_prefix" {
+  description = "Domain prefix, e.g. www, staging etc."
+  default = "dev"
+}
+
+variable "r53_notes_zone_id" {
+  description = "DNS zone ID for 'notes' services"
+}
+
