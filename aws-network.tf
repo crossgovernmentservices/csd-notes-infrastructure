afb6527f1badacaafdaf601406aec3327fdfb29b
Kerin Cosford <cosford@gmail.com>
2016-04-05 09:57:59 +0100

    Renamed network resources

--- aws-network.tf
+++ aws-network.tf
@@ -6,9 +6,12 @@ provider "aws" {
 # Create a VPC to launch our instances into
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
   tags {
-    Name = "performance-review"
+    Name = "notes-${var.environment}"
+    Environment = "${var.environment}"
   }
 }
 
@@ -18,7 +21,8 @@ resource "aws_subnet" "public-a" {
   cidr_block              = "10.0.1.0/24"
   availability_zone = "eu-west-1a"
   tags {
-    Name = "performance-review-public"
+    Name = "notes-${var.environment}-public-a"
+    Environment = "${var.environment}"
   }
 }
 
@@ -28,7 +32,8 @@ resource "aws_subnet" "public-b" {
   cidr_block              = "10.0.2.0/24"
   availability_zone = "eu-west-1b"
   tags {
-    Name = "performance-review-public"
+    Name = "notes-${var.environment}-public-b"
+    Environment = "${var.environment}"
   }
 }
 
@@ -38,7 +43,8 @@ resource "aws_subnet" "public-c" {
   cidr_block              = "10.0.3.0/24"
   availability_zone = "eu-west-1c"
   tags {
-    Name = "performance-review-public"
+    Name = "notes-${var.environment}-public-c"
+    Environment = "${var.environment}"
   }
 }
 
@@ -48,7 +54,8 @@ resource "aws_subnet" "private-a" {
   cidr_block              = "10.0.10.0/24"
   availability_zone = "eu-west-1a"
   tags {
-    Name = "performance-review-private"
+    Name = "notes-${var.environment}-private-a"
+    Environment = "${var.environment}"
   }
 }
 
@@ -58,7 +65,8 @@ resource "aws_subnet" "private-b" {
   cidr_block              = "10.0.11.0/24"
   availability_zone = "eu-west-1b"
   tags {
-    Name = "performance-review-private"
+    Name = "notes-${var.environment}-private-b"
+    Environment = "${var.environment}"
   }
 }
 
@@ -68,7 +76,8 @@ resource "aws_subnet" "private-c" {
   cidr_block              = "10.0.12.0/24"
   availability_zone = "eu-west-1c"
   tags {
-    Name = "performance-review-private"
+    Name = "notes-${var.environment}-private-c"
+    Environment = "${var.environment}"
   }
 }
 
@@ -77,7 +86,8 @@ resource "aws_internet_gateway" "default" {
   vpc_id = "${aws_vpc.default.id}"
 
   tags {
-    Name = "performance-review"
+    Name = "notes-${var.environment}"
+    Environment = "${var.environment}"
   }
 }
 
@@ -94,6 +104,11 @@ resource "aws_route_table" "private-nat-a" {
     cidr_block = "0.0.0.0/0"
     nat_gateway_id = "${aws_nat_gateway.gw-a.id}"
   }
+
+  tags {
+    Name = "notes-${var.environment}-private-nat-a"
+    Environment = "${var.environment}"
+  }
 }
 
 resource "aws_route_table" "private-nat-b" {
@@ -102,6 +117,11 @@ resource "aws_route_table" "private-nat-b" {
     cidr_block = "0.0.0.0/0"
     nat_gateway_id = "${aws_nat_gateway.gw-b.id}"
   }
+
+  tags {
+    Name = "notes-${var.environment}-private-nat-b"
+    Environment = "${var.environment}"
+  }
 }
 
 resource "aws_route_table" "private-nat-c" {
@@ -109,6 +129,11 @@ resource "aws_route_table" "private-nat-c" {
   route {
     cidr_block = "0.0.0.0/0"
     nat_gateway_id = "${aws_nat_gateway.gw-c.id}"
+  }
+
+  tags {
+    Name = "notes-${var.environment}-private-nat-c"
+    Environment = "${var.environment}"
   }
 }
 
