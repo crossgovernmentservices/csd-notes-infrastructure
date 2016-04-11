0da03725e19eac6b254bc75231f36e78da592fa2
Kerin Cosford <cosford@gmail.com>
2016-04-11 17:11:19 +0100

    jenkins.sh now takes args for apply|destroy and env name

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+
+: ${1?"Usage: $0 apply|destroy ENV"}
+
+case "$1" in
+    apply) ACTION=apply ;;
+    destroy) ACTION=destroy ;;
+esac
+
+ENV=${2:-dev}
+
 cd csd-notes-config && blackbox_postdeploy
-DB_USER=$(grep DB_USERNAME envs/dev.env | sed -E 's/(.*)=(.*)/\2/')
-DB_PASSWORD=$(grep DB_PASSWORD envs/dev.env | sed -E 's/(.*)=(.*)/\2/')
+DB_USER=$(grep DB_USERNAME envs/${ENV}.env | sed -E 's/(.*)=(.*)/\2/')
+DB_PASSWORD=$(grep DB_PASSWORD envs/${ENV}.env | sed -E 's/(.*)=(.*)/\2/')
 
 cd ../csd-notes-infrastructure && blackbox_postdeploy
 
 terraform remote config -backend=s3 -backend-config="bucket=csd-notes-terraform"\
-  -backend-config="key=dev.tfstate" -backend-config="region=eu-west-1"
+  -backend-config="key=${ENV}.tfstate" -backend-config="region=eu-west-1"
 
-terraform apply  -var "rds_username=${DB_USER}" -var "rds_password=${DB_PASSWORD}"\
-  -var "environment=dev" -var "domain_prefix=dev"
+terraform ${ACTION} -var "rds_username=${DB_USER}" -var "rds_password=${DB_PASSWORD}"\
+  -var "environment=${ENV}" -var "domain_prefix=${ENV}"
