8d0a81d9992cc71d603460552d8628b6493ff6e0
Kerin Cosford <cosford@gmail.com>
2016-04-11 16:49:11 +0100

    Removed 'shared' update from jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -5,13 +5,8 @@ DB_PASSWORD=$(grep DB_PASSWORD envs/dev.env | sed -E 's/(.*)=(.*)/\2/')
 
 cd ../csd-notes-infrastructure && blackbox_postdeploy
 
-cd shared
-terraform remote config -backend=s3 -backend-config="bucket=csd-notes-terraform"\
-  -backend-config="key=shared/terraform.tfstate" -backend-config="region=eu-west-1"
-terraform apply
-
-cd ..
 terraform remote config -backend=s3 -backend-config="bucket=csd-notes-terraform"\
   -backend-config="key=dev.tfstate" -backend-config="region=eu-west-1"
+
 terraform apply  -var "rds_username=${DB_USER}" -var "rds_password=${DB_PASSWORD}"\
   -var "environment=dev" -var "domain_prefix=dev"
