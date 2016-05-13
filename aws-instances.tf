e4934fd1ae5e9ad0aafea66f09908e3a911b5f49
Kerin Cosford <cosford@gmail.com>
2016-05-13 15:13:22 +0100

    Added jenkins' public key to ssh bastion via aws user_data

--- aws-instances.tf
+++ aws-instances.tf
@@ -6,6 +6,10 @@ resource "aws_instance" "ssh-bastion" {
   subnet_id = "${aws_subnet.public-a.id}"
   vpc_security_group_ids = ["${aws_security_group.ssh-bastion.id}"]
   associate_public_ip_address = true
+  user_data = <<EOF
+#!/bin/bash
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDL/56dxiNa40Fp6Ii9fOafsWzjOGY2c6rOBndXVadmzivFkvnsU/ov3oMvKG+tYS5RrBFIfx1Al+L/Le9FCFUOBwJofoV2fKlr1WZLzAz3nND36Nbbsf8JNQ7uQeEAYED5mJUScAPhdzHANL43KBDM/rgdb+lJtenLFw0U5XzPKJmPybzTZIURmG5wA4cNIWkGZQsCMRQhJ5vTFUkqmLG29YrsxON0MQS0S4G+Jnu62MX/yPb0CjqF1w4thly4gUA4y/uZN788nZFb6yHDDJLCzFfn7bL8+F/qbPWw6OSc7TsMF6RW37eIGLTGnMTP7GLjv6NnxKQMKYM1wh9dxHlB1pOS6NMSNBUX0/E2O4k3hD52M+iAtbalQzm/iMu7DuI09c/GN99QzODdnJMdwBUUQYXZ4MWMbNJfGdtO62sGYYIChcIEcQoUHytY+yxhrit0Z4DqwL+hB8Yv6u2vU7mpkFH+rPAq2nZhFHRW58CSECWT2kpkmy4h+yGi8VPeczY6qQjr0RjKym7Ee5Td7Yia5dkReG5f4Xx5ZXs/rcr4oNEri3JW3z6gXmgNvqlG53s1OxwCBtwULCnVu9lKcMLr5+GgSYDd0oFzdtDGDrTv17G+AQ/32X4w68LilYXDKvSmW3zkbAvud9SE0NzRgJ887A2bsaYoYdogiuij5m8t3Q==" >> /home/ubuntu/.ssh/authorized_keys
+EOF
 
   tags {
     Name = "notes-ssh-${var.environment}"
