751e0246cf4e290fa29feedd9e1f3826008249a2
Kerin Cosford <cosford@gmail.com>
2016-05-13 15:13:44 +0100

    added dns record for ssh bastion

--- aws-dns.tf
+++ aws-dns.tf
@@ -10,3 +10,12 @@ resource "aws_route53_record" "public-a" {
     evaluate_target_health = true
   }
 }
+
+# SSH A record
+resource "aws_route53_record" "ssh-a" {
+  zone_id = "${terraform_remote_state.csd.output.r53_notes_zone_id}"
+  name = "ssh.${var.domain_prefix}"
+  type = "A"
+  ttl = "300"
+  records = ["${aws_instance.ssh-bastion.public_ip}"]
+}
